d5250a380ab5b54a8579338fe602780db64487ba
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-04-28 10:40:05 +0200

    Spring Project now is connected to PostgreSQL local database

--- src/main/java/com/urbancore/urbancore_api/UrbancoreApiApplication.java
+++ src/main/java/com/urbancore/urbancore_api/UrbancoreApiApplication.java
@@ -2,10 +2,9 @@ package com.urbancore.urbancore_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class ,DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class UrbancoreApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/urbancore/urbancore_api/models/User.java
@@ -0,0 +1,22 @@
+package com.urbancore.urbancore_api.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String firebaseUid;
+
+    private String email;
+
+    private String role;
+}
