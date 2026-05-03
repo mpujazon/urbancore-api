7b62444190febcdcd603bff7fb59e17c5a7bc907
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-03 20:28:56 +0200

    Declared findAllByOrderByCreatedAtDesc method at IncidentRepository

--- src/main/java/com/urbancore/urbancore_api/repositories/IncidentRepository.java
+++ src/main/java/com/urbancore/urbancore_api/repositories/IncidentRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IncidentRepository extends JpaRepository<Incident, String> {
+    List<Incident> findAllByOrderByCreatedAtDesc();
     List<Incident> findAllByReporterIdOrderByCreatedAtDesc(Long reporterId);
 }
