eba159b65cb83d9b4dda888cf6e8838dec5a5855
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-03 18:15:20 +0200

    Endpoint GET /api/incidents/me created in IncidentController

--- src/main/java/com/urbancore/urbancore_api/controllers/IncidentController.java
+++ src/main/java/com/urbancore/urbancore_api/controllers/IncidentController.java
@@ -6,14 +6,17 @@ import com.urbancore.urbancore_api.services.IncidentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping({"/api/incidents", "/incidents"})
+@RequestMapping({"/api/incidents"})
 public class IncidentController {
 
     private final IncidentService incidentService;
@@ -29,5 +32,10 @@ public class IncidentController {
             @AuthenticationPrincipal Jwt jwt
     ) {
         return incidentService.createIncident(request, jwt);
+    }
+
+    @GetMapping("/me")
+    public List<IncidentDto> getMyIncidents(@AuthenticationPrincipal Jwt jwt) {
+        return incidentService.getCurrentCitizenIncidents(jwt);
     }
 }
