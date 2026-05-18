50555094d54fffbfcb159af963f9d12748b9f700
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-05-18 11:05:34 +0200

    PlannedActionRepository created

---
+++ src/main/java/com/urbancore/urbancore_api/repositories/PlannedActionRepository.java
@@ -0,0 +1,18 @@
+package com.urbancore.urbancore_api.repositories;
+
+import com.urbancore.urbancore_api.models.Incident;
+import com.urbancore.urbancore_api.models.PlannedActionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import com.urbancore.plannedactions.domain.PlannedAction;
+
+public interface PlannedActionRepository extends JpaRepository<PlannedAction, UUID> {
+    List<PlannedAction> findByIncident(UUID incident);
+    List<PlannedAction> findByIncidentCityIdAndScheduledStartBetween(UUID cityId, Instant from, Instant to);
+    
+    boolean existsByIncidentIdAndStatusNot(UUID incidentId, PlannedActionStatus status);
+}
